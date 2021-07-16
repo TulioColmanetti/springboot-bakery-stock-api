8b6203871b5ded2371b3e6f207d4aeebdf13dbd9
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-16 13:09:25 -0300

    Initial commit

---
+++ src/main/java/com/projects/dev/tulio/springbootbakerystockapi/SpringbootBakeryStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.projects.dev.tulio.springbootbakerystockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBakeryStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBakeryStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/dev/tulio/springbootbakerystockapi/SpringbootBakeryStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.dev.tulio.springbootbakerystockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBakeryStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
