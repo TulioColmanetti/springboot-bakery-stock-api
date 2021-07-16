24ee237f97539fb79af7c1f9146c001d3920db99
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-16 13:53:59 -0300

    Entity sketch.

---
+++ src/main/java/com/projects/dev/tulio/springbootbakerystockapi/entity/Product.java
@@ -0,0 +1,20 @@
+package com.projects.dev.tulio.springbootbakerystockapi.entity;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    private String size;
+
+    private double cost;
+
+    private int max;
+
+    private int quantity;
+}
