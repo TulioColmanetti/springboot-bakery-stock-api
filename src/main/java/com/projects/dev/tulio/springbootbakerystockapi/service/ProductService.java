1ea9cc63afa0053154eb2176872bcf6f2d61c032
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-16 18:39:10 -0300

    Next TDD steps: After test failed, write minimal code to pass the test and re-run it.

--- src/main/java/com/projects/dev/tulio/springbootbakerystockapi/service/ProductService.java
+++ src/main/java/com/projects/dev/tulio/springbootbakerystockapi/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.projects.dev.tulio.springbootbakerystockapi.service;
 
+import com.projects.dev.tulio.springbootbakerystockapi.entity.Product;
 import com.projects.dev.tulio.springbootbakerystockapi.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private ProductRepository productRepository;
+
+    public Product createProduct(Product expectedSavedProduct) {
+        Product savedProduct = productRepository.save(expectedSavedProduct);
+        return savedProduct;
+    }
 }
