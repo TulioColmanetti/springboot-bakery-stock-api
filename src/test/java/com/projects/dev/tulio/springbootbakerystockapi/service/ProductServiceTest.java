08086c2608dfb6d125fc29088290223c7d962ddb
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-19 23:53:39 -0300

    TDD 1 and 2: Add new test and fail it for checking if Product already exists before creating.

--- src/test/java/com/projects/dev/tulio/springbootbakerystockapi/service/ProductServiceTest.java
+++ src/test/java/com/projects/dev/tulio/springbootbakerystockapi/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package com.projects.dev.tulio.springbootbakerystockapi.service;
 import com.projects.dev.tulio.springbootbakerystockapi.builder.ProductDTOBuilder;
 import com.projects.dev.tulio.springbootbakerystockapi.dto.ProductDTO;
 import com.projects.dev.tulio.springbootbakerystockapi.entity.Product;
+import com.projects.dev.tulio.springbootbakerystockapi.exception.ProductAlreadyRegisteredException;
 import com.projects.dev.tulio.springbootbakerystockapi.mapper.ProductMapper;
 import com.projects.dev.tulio.springbootbakerystockapi.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -28,12 +31,13 @@ class ProductServiceTest {
     ProductService productService;
 
     @Test
-    void whenProductInformedThenItShouldBeCreated(){
+    void whenProductInformedThenItShouldBeCreated() throws ProductAlreadyRegisteredException {
 //        given
         ProductDTO expectedProductDTO = ProductDTOBuilder.builder().build().toProductDTO();
         Product expectedSavedProduct = productMapper.toModel(expectedProductDTO);
 
 //        when
+        when(productRepository.findByName(expectedProductDTO.getName())).thenReturn(Optional.empty());
         when(productRepository.save(expectedSavedProduct)).thenReturn(expectedSavedProduct);
 
 //        then
